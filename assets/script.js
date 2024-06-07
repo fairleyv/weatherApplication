cc93526de253876a6852a862244e960f3a6d1dce
fairleyv <fairleyvincent@gmail.com>
2024-06-07 13:45:20 -0700

    added a handleFormSubmit to get form data on submit

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,14 @@
-require('dotenv').config();
-
-const apiKey = process.env.API_KEY
+const apiKey = '56b5bdd21a24df22d4d2e2db56542cf0'
 
 let currentCity = document.getElementById('searchedCityName');
+
+function handleFormSubmit(event) {
+    event.preventDefault();
+
+    const cityQuery = document.getElementById('cityQuery').value;
+    getWeather(cityQuery);
+
+    return false;}
 
 function getWeather(city) {
     let weatherAPIURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=imperial`
@@ -14,4 +20,4 @@ function getWeather(city) {
     })
 };
 
-getWeather('Detroit')
+// getWeather('Detroit')
