b38d39aaab5082465edad655777ed06d1e10749b
fairleyv <fairleyvincent@gmail.com>
2024-06-07 13:22:43 -0700

    created getWeather function. Currently testing

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,17 @@
 require('dotenv').config();
+
+const apiKey = process.env.API_KEY
+
+let currentCity = document.getElementById('searchedCityName');
+
+function getWeather(city) {
+    let weatherAPIURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=imperial`
+    fetch(weatherAPIURL)
+    .then(response => response.json())
+    .then((data) => {
+        console.log(data.city)
+        currentCity.textContent = (data.city.name)
+    })
+};
+
+getWeather('Detroit')
